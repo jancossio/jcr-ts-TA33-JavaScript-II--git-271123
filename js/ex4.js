dfae1f6501daec1dcfd87fdabd55636aab4b0128
jancossio <jancossio@gmail.com>
2023-12-04 22:59:51 +0100

    actualizacion calculadora

--- js/ex4.js
+++ js/ex4.js
@@ -2,49 +2,81 @@ var pantalla = document.getElementById("pantalla");
 var tmp_valor = "";
 var operador1 = "";
 var signoOperante = "";
+let escribir = false;
 
 function agregarValor(valor){
-    pantalla.innerHTML += valor;
+    if(signoOperante === ''){
+        pantalla.innerHTML += valor;
+    }else if(!escribir){
+        pantalla.innerHTML = "";
+        pantalla.innerHTML += valor;
+        escribir=true;
+    }else{
+        pantalla.innerHTML += valor;
+    }
 }
 
 function borrarTodo(){
     tmp_valor = "";
     pantalla.innerHTML = "";
+    operador1 = "";
 }
 
 
 function operacion(operacion){ 
     operador1 = pantalla.innerHTML;
     signoOperante = operacion;
-    pantalla.innerHTML="";
 }
 
 function resultado(){
 
-    let operador2 = pantalla.innerHTML;
+    let operador2 = parseFloat(pantalla.innerHTML);
 
     switch (signoOperante) {
         case "+":
-            result = suma(operador1, operador2);
+            pantalla.innerHTML = operador1 + operador2;
             break;
             
         case "-":
-            result = resta(operador1, operador2);
+            pantalla.innerHTML =  operador1 - operador2;
             break;
             
         case "x":
-            result = multiplicacion(operador1, operador2);
+            pantalla.innerHTML =  operador1 * operador2;
             break;
             
-        case "/":
-                
+        case "/":     
+            if (operador2 == 0.0) {
+                pantalla.innerHTML="INFINITY";
+            }else{
+                pantalla.innerHTML =  operador1 / operador2;
+            }
+            break;
+        case "Raiz":  
+            if (operador2 > 0.0) {
+                pantalla.innerHTML="Error";
+            }
+            pantalla.innerHTML = Math.sqrt(operador2);
+            break;
+
+        case "1/x":  
             if (operador2 == 0.0) {
                 pantalla.innerHTML="INFINITY";
             }
-            result = division(operador1, operador2);
-
-            
+            pantalla.innerHTML = (1/operador2);
             break;
+
+            case "%":  
+            if (operador2 == 0.0) {
+                pantalla.innerHTML="INFINITY";
+            }
+            pantalla.innerHTML = (operador1/100)*operador2;
+            break;
+
+            case "+/-":  
+            pantalla.innerHTML = -operador2;
+            break;
+            
         default:
             System.out.println("Error");
         }
@@ -63,5 +95,9 @@ function multiplicacion( op1, op2) {
 }
 	
 function division( op1, op2) {
+	pantalla.innerHTML =  op1 / op2;
+}
+
+function raiz( op1, op2) {
 	pantalla.innerHTML =  op1 / op2;
 }
